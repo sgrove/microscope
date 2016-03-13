fd4517779c373d0c2016ef00dc8ce8545cb845d3
Sean Grove <sean@bushi.do>
2016-03-13 13:44:36 -0700

    Add Yojson as a dependency (I think)

--- bin/build_client.sh
+++ bin/build_client.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "Rebuilding bytecode"
-ocamlfind ocamlc -g -package js_of_ocaml -package js_of_ocaml.syntax -package js_of_ocaml.ppx -linkpkg -o client.byte src/client/main.ml
+ocamlfind ocamlc -thread -g -package js_of_ocaml -package js_of_ocaml.syntax -package js_of_ocaml.ppx -package core -package yojson -package ppx_deriving_yojson -linkpkg -o client.byte src/client/main.ml
 echo "Outputting js"
-js_of_ocaml --source-map --pretty --no-inline --debug-info -o resources/public/js/client.js client.byte
+js_of_ocaml --disable genprim --source-map --pretty --no-inline --debug-info -o resources/public/js/client.js +weak.js +toplevel.js client.byte
 echo "Done"
